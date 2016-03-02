ea73514526bdbc437d6dfb58f7cb7b436c07d2f1
Jason Cheung <jasoncheungcf@gmail.com>
2016-03-02 11:26:59 -0800

    initial commit

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 var express = require('express')
-
 var app = express ();
+var routes = require('./server/routes');
+
+routes(app);
+
+var port = process.env.PORT || 3000
+
+app.listen(port,function(){
+  console.log('Server Established. Running on port '+port);
+})
---
+++ server/controller.js
@@ -0,0 +1,8 @@
+module.exports  = {
+
+  getResult:function(req,res) {
+
+    res.send('response from getResult');
+
+  }
+}
