c5bdc1d154ab73186b454e57603052f23f7d40d3
Zak Golding <hello@zakarhino.com>
2016-03-02 21:07:21 -0800

    Fixed formatting

--- server/controller.js
+++ server/controller.js
@@ -3,12 +3,12 @@ const request = require('request');
 const Correlation = require('node-correlation');
 const qs = require('querystring');
 
-module.exports  = {
-  getResult:function(req,res) {
+module.exports = {
+  getResult: function(req, res) {
     console.log("req keyword url:", req.query.keyword);
     let tempObject = {
       companyName: 'google',
-      tempArray: [ 100,
+      tempArray: [100,
         100,
         100,
         100,
@@ -31,29 +31,30 @@ module.exports  = {
         81.62162162162161,
         81.62162162162161,
         81.62162162162161,
-        81.62162162162161 ]
-      };
+        81.62162162162161
+      ]
+    };
     let keyword = req.query.keyword;
-    let url = 'http://www.google.com/trends/fetchComponent?hl=en-US&geo=US&q='+keyword+'&cid=TIMESERIES_GRAPH_0&export=3';
+    let url = 'http://www.google.com/trends/fetchComponent?hl=en-US&geo=US&q=' + keyword + '&cid=TIMESERIES_GRAPH_0&export=3';
 
-    return res.send({companyName: tempObject.companyName, corr: tempObject.tempArray});
+    return res.send({ companyName: tempObject.companyName, corr: tempObject.tempArray });
 
-    request.get(url, function(err,response,body) {
-      if(err) {
+    request.get(url, function(err, response, body) {
+      if (err) {
         res.send('Error making a get request to google trends');
       } else {
         console.log('the request return with', body.slice(61));
         let info = eval(body.slice(61));
         let results = [];
-        for(var i = 26; i > 2; i--) {
-          results.push(info.table.rows[info.table.rows.length-i].c[1].v);
+        for (var i = 26; i > 2; i--) {
+          results.push(info.table.rows[info.table.rows.length - i].c[1].v);
         }
-        let max = 100/Math.max.apply(Math,results);
+        let max = 100 / Math.max.apply(Math, results);
         let scaledArray = results.map(function(result) {
-          return result*max;
+          return result * max;
         });
-        let corr = Correlation.calc(tempObject.tempArray,scaledArray);
-        res.send({companyName: tempObject.companyName, corr: corr});
+        let corr = Correlation.calc(tempObject.tempArray, scaledArray);
+        res.send({ companyName: tempObject.companyName, corr: corr });
       }
     });
 
