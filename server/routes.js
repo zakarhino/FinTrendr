fbb136dd5ac5d074bfa466616399ebdfbee1f2c6
Jason Cheung <jasoncheungcf@gmail.com>
2016-03-02 17:28:10 -0800

    [Bugifx] fixed serve public document

--- server/routes.js
+++ server/routes.js
@@ -6,17 +6,17 @@ var bodyParser = require('body-parser');
 var KeywordController = require('./controller.js');
 
 module.exports = function(app) {
-
   /**
    * log all incoming request URL.
    */
+  console.log(__dirname)
   app.all('*', function(req, res, next) {
     console.log(req.method, 'Request for URL ', req.url)
     next();
   });
 
   app.use(bodyParser.json());
-  app.use(express.static(__dirname + '../public'));
+  app.use(express.static(__dirname + '/../public'));
 
   /**
    * Keyword API get Request
