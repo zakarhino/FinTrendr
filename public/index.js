bd6c4c9be075573556c0b7e41328060e9af03e60
Jason Cheung <jasoncheungcf@gmail.com>
2016-03-05 15:00:02 -0800

    (CSS) added css to homepage

--- public/index.js
+++ public/index.js
@@ -1,21 +1,22 @@
 $(function() {
-  // $('#buttonSubmit').on('click', function() {
-  //   console.log('on click yo');
-  //   $.ajax({
-  //     type: 'GET',
-  //     url: '/api/',
-  //     contentType: 'application/json',
-  //     data: {
-  //       keyword: document.getElementById('keyword').value
-  //     },
-  //     error: function(req, status, err) {
-  //       $('#info').html('<p>An error has occurred:' + status + '-' + err + '</p>');
-  //     },
-  //     success: function(data) {
-  //       data.corr.forEach((dataObj) => {
-  //         $('#stage').append('<div>' + document.getElementById('keyword').value + ' correlations are: ' + dataObj.keyword + ': ' + dataObj.correlation + '</div>');
-  //       });
-  //     }
-  //   });
-  // });
+  $('#buttonSubmit').on('click', function() {
+    console.log('on click yo');
+    $.ajax({
+      type: 'GET',
+      url: '/api/',
+      contentType: 'application/json',
+      data: {
+        keyword: document.getElementById('keyword').value
+      },
+      error: function(req, status, err) {
+        $('#info').html('<p>An error has occurred:' + status + '-' + err + '</p>');
+      },
+      success: function(data) {
+        data.corr.forEach((dataObj,i) => {
+           var id = "#keyword"+(i+1)
+          $(id).html('<div> correlations are: ' + dataObj.keyword + ': ' + dataObj.correlation + '</div>');
+        });
+      }
+    });
+  });
 });
