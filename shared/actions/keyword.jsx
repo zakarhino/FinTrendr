1e162f22276573fdc2dcce530987b67125971e76
Eric Mustin <mustin.eric@gmail.com>
2016-03-09 20:33:38 -0500

    (searchbar) added onsubmit fucntion that invokes an action creator getKeyword

---
+++ shared/actions/keyword.jsx
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export const GET_KEYWORD = 'GET_KEYWORD';
+
+export function getKeyword(keyword) {
+  //add in the logic for getting  the return
+  //from the db.getkeyword function (keyword and data)
+  // in /api/ route then pass that as the payload
+  //this will require a redux-promise
+
+  return {
+    type: GET_KEYWORD,
+    payload: {Keyword: "obama",
+              data: [{"Feb2014":0},{"Mar2014":1},{"Apr2014":2},{"May2014":3},{"Jun2014":4},{"Jul2014":5},{"Aug2014":6},{"Sep2014":7},{"Oct2014":8},{"Nov2014":9},{"Dec2014":10},{"Jan2015":11},{"Feb2015":12},{"Mar2015":13},{"Apr2015":14},{"May2015":15},{"Jun2015":16},{"Jul2015":17},{"Aug2015":18},{"Sep2015":19},{"Oct2015":20},{"Nov2015":21},{"Dec2015":22},{"Jan2016":23}]
+            }
+  };
+}
