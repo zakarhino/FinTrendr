06c3b5dca4da57529747fc19b23ed660d316d6dd
Jason Cheung <jasoncheungcf@gmail.com>
2016-03-15 10:03:24 -0700

    (Bugfix) fix the testing suite

--- test/controller.test.js
+++ test/controller.test.js
@@ -1,13 +1,13 @@
-var should = require('should');
-var db = require('../server/db/db-model.js');
-var config = require('../utility/common').config();
-var controller = require('../server/controller.js');
-var request = require('request');
-var Correlation = require('node-correlation');
-var qs = require('querystring');
-var tempData = require('./testresponse.js');
+"use strict"
+const should = require('should');
+const db = require('../server/db/db-model.js');
+const config = require('../utility/common').config();
+const controller = require('../server/controller.js');
+const request = require('request');
+const Correlation = require('node-correlation');
+const qs = require('querystring');;
 
-var seraph = require("seraph")({
+const seraph = require("seraph")({
   server: config.neo4j.server,
   user: config.neo4j.user,
   pass: config.neo4j.password
@@ -20,28 +20,19 @@ function wipeDB() {
 }
 
 describe('Server Controller', function() {
-
+  it("should contain controller object", function() {
+    should.exist(controller);
+  });
 
   it('Should contain getResult and all utility functions for parsing', function() {
-    controller.queryGtrends.should.be.a.Function();
-    controller.convertGtrends.should.be.a.Function();
     controller.createResultsObject.should.be.a.Function();
-  });
-  
-  describe('convertGtrends', function() {
-
-    var tempGoogleResponse = eval(tempData.tempString.slice(61));
-    it('should return an array', function() {
-      var x = controller.convertGtrends(tempGoogleResponse);
-      x.should.be.an.instanceOf(Array);
-    });
   });
 
   // describe('createResultsObject', function() {
   //   it('should return an object', function() {
   //     // return db.getKeyword({}).should.be.fulfilled().then(function(data) {
   //     //   console.log(data);
-  //     var tempData = [{Keyword: 'bobbert', 
+  //     var tempData = [{Keyword: 'bobbert',
   //     date:['{"a":100}','{"b":100}','{"c":100}','{"d":100}','{"e":100}','{"f":100}','{"g":100}','{"h":100}','{"i":100}','{"j":100}','{"k":100}','{"l":100}']}];
   //     console.log(typeof tempData);
   //     var x = controller.createResultsObject(tempData);
@@ -49,4 +40,5 @@ describe('Server Controller', function() {
   //     x.should.be.an.instanceOf(Object);
   //   });
   // });
+
 });
