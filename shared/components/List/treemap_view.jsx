87b8fce34557de2c86d3aff99369fbd5479d66ec
Jason Cheung <jasoncheungcf@gmail.com>
2016-03-15 19:27:40 -0700

    (Treemap) initial setup for treemap

--- shared/components/List/treemap_view.jsx
+++ shared/components/List/treemap_view.jsx
@@ -1,21 +1,68 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {AreaChart} from 'react-d3';
-export class Graph extends Component {
-  componentWillMount(){
-    console.log('treemap loaded');
+import {Treemap} from 'react-d3';
+import {bindActionCreators} from 'redux';
+import {getStocksInfo} from '../../actions/stocks';
+class TreeMap extends Component {
+  componentWillMount() {
+    if (this.props.keyword) {
+      console.log('Treemap is Mounting');
+      this
+        .props
+        .getStocksInfo(this.props.keyword);
+    }
+  }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.keyword !== this.props.keyword) {
+      if (nextProps.keyword) {
+        this
+          .props
+          .getStocksInfo(nextProps.keyword);
+      }
+    }
+  }
+  convertToGraphData(data) {
+    let result = []
+    for (let item in data) {
+      let temp = [];
+      for (let i = 0; i < 20; i++) {
+        temp.push(data[item][i]);
+      }
+      result.push(temp);
+    }
+    console.log('converting data', result);
+    return result;
   }
   render() {
+    let {stocks} = this.props;
+    let graphData = this.convertToGraphData(stocks);
+    let colorFunction = d3
+      .scale
+      .linear()
+      .domain([-1, 0, 1])
+      .range(['red', 'white', 'green']);
+    let colorAccessor = function(d) {
+      if (d.parent && d.parent.name === 'negative') {
+        return -d.value;
+      } else {
+        return d.value;
+      };
+    }
+
     return (
       <div>
-        Graph ish 
+        <Treemap data={graphData} colors={colorFunction} colorAccessor={colorAccessor} width={1000} height={300} textColor="#484848" fontSize="12px" title="Treemap" hoverAnimation={true}/>
       </div>
     );
   }
 }
 function mapStateToProps(state) {
-  console.log('Graph receive new state update')
-  return {currentKeyword: state.keyword.current}
+  return {keyword: state.keyword.current, stocks: state.stocks.items};
 }
-
-  export default connect(mapStateToProps)(Graph);
+function mapDispatchToProps(dispatch) {
+  let obj = {
+    getStocksInfo: getStocksInfo
+  };
+  return bindActionCreators(obj, dispatch);
+};
+export default connect(mapStateToProps, mapDispatchToProps)(TreeMap);
