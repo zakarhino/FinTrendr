bca1a01e6d59024cab6b639ae3c5535fad165e92
Eric Mustin <mustin.eric@gmail.com>
2016-03-16 22:10:40 -0400

    (feature) added hottrends to state

--- server/hotTrendsController.js
+++ server/hotTrendsController.js
@@ -2,6 +2,7 @@
 
 const request = require('request');
 const qs = require('querystring');
+const popularController = require('./popularController.js');
 
 module.exports = {
   getHotTrendsInfo: function(req, res) {
@@ -9,9 +10,36 @@ module.exports = {
       if (err) {
         res.send('')
       } else {
-        let bodyParsed = JSON.parse(body);
-        console.log(bodyParsed[1]);
-        res.send(bodyParsed[1]);
+        request.get('https://api.stocktwits.com/api/2/trending/symbols/equities.json', (err, response, twitsBody) => {
+          if (err) {
+            res.send('');
+          } else {
+            let bodyParsed = JSON.parse(body);
+            let twitsBodyParsed = JSON.parse(twitsBody);
+            let twitSymbols = twitsBodyParsed.symbols.map((symbolObj) => {
+              return symbolObj.symbol;
+            });
+            console.log(bodyParsed[1]);
+
+            popularController.getPopularInfo().then((randomTen) => {
+                console.log('outside the db');
+                res.send({
+                  keywords: bodyParsed[1],
+                  stocks: twitSymbols,
+                  randomTen: randomTen
+                });
+              })
+              .catch((err) => {
+                console.log('err is', err);
+                res.send({
+                  keywords: bodyParsed[1],
+                  stocks: twitSymbols
+
+                });
+
+              });
+          }
+        });
       }
     });
   }
