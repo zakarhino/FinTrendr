f2335f5b0a3df7fb808ec9cffcfb013ba761f4ad
Eric Mustin <mustin.eric@gmail.com>
2016-03-22 00:52:55 -0400

    (feature) added tooltips and transitions to news

--- shared/components/news/news_list.jsx
+++ shared/components/news/news_list.jsx
@@ -3,9 +3,11 @@ import {parseString} from 'xml2js';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {getNews} from '../../actions/news';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import ReactToolTip from 'react-tooltip';
+
 class NewsList extends Component {
   componentWillMount() {
-
   }
   componentWillReceiveProps(nextProps) {
     if (this.props.term.Keyword !== nextProps.term.Keyword) {
@@ -13,25 +15,33 @@ class NewsList extends Component {
     }
   }
   renderArticles() {
+    console.log(this.props.news);
+    let count = 0;
     return this.props.news.map((article) => {
+      count++;
       return (
-        <li key={article.guid} className="list-group-item">
+        <li key={article.guid}  className="list-group-item" data-tip data-for={`article-${count}`}>
           <p><a href={article.link} target="_blank">{article.title}</a></p>
           <p>{article.pubDate}</p>
+          <ReactToolTip id={`article-${count}`} type='error'>
+            {article.contentSnippet}
+          </ReactToolTip>
         </li>
       );
     });
   }
   render() {
-    console.log(this.props.news);
     return (
       <div className="news spacer">
         <img src="/img/News.png" width="40" className="pull-xs-left" />
         <h3>  Related News:</h3>
-        <ul className="list-group">
-          {this.renderArticles()}
-        </ul>
+          <ul className="list-group">
+          <ReactCSSTransitionGroup transitionName="newsExample" transitionEnterTimeout={500} transitionLeaveTimeout={300} transitionAppear={true}>
+            {this.renderArticles()}
+            </ReactCSSTransitionGroup>
+          </ul>
       </div>
+      
     );
   }
 };
