a28fd542f597ae0cde1115867b6cafdb72a8685d
Eric Mustin <mustin.eric@gmail.com>
2016-03-22 20:02:01 -0400

    (feature) alchemy validation of articles

--- alchemy/app.js
+++ alchemy/app.js
@@ -88,94 +88,48 @@ var alchemyapi = new AlchemyAPI();
 //  });
 // }
 var count = 0;
-module.exports = function keywords(keyword, item, cb) {
-  var keywordList = [];
-  var parsedString;
-  var keyword1 = keyword;
-  var keyword2 = item;
-  count = count + 1;
-  console.log(count);
-  setTimeout(function() {
-    request.get('http://rss2json.com/api.json?rss_url=https%3A%2F%2Fnews.google.com%2Fnews%3Fq%3D' + keyword1 + '%20' + keyword2 + '%26output%3Drss', function(err, response, body) {
-      if(err) {
-        cb(err);
+module.exports = function keywords(keyword, item, url) {
+  return new Promise(function(resolve, reject) {
+
+    alchemyapi.keywords('url', url, { 'sentiment': 0 }, function(response, err, err2) {
+
+      var tempData = response;
+      if (tempData.status === 'ERROR') {
+        resolve(0);
       }
-      var parsed = JSON.parse(body);
-      console.log(body);
-      if(parsed.status === 'error') {
-        cb('rate limited');
-        return;
-      }
-      var mapped = parsed.items.map(function(obj) {
-        return obj.link.substr(obj.link.indexOf('url=') + 4);
-      });
-
-      mapped.forEach(function(url) {
-        var promise = new Promise(function(resolve, reject) {
-
-          alchemyapi.keywords('url', url, { 'sentiment': 0 }, function(response, err, err2) {
-            console.log(response, "error is: ", err, err2);
-
-            var tempData = response;
-            if (tempData.status === 'ERROR') {
-              resolve(0);
-            }
-            if (tempData.keywords) {
-              var tempKeywordList = [];
-                // console.log('tempData for :', i, " is ", tempData.keywords);
-              var tempData2 = tempData.keywords;
-              tempData2.forEach(function(obj) {
-                if (obj.text) {
-                  tempKeywordList.push(obj.text);
-                }
-              });
-              var tempKeywordList2 = tempKeywordList.toString().toLowerCase();
-              console.log(tempKeywordList2.includes(keyword1));
-              console.log(tempKeywordList2.includes(keyword2));
-              if (tempKeywordList2.includes(keyword1)) {
-                if (tempKeywordList2.includes(keyword2)) {
-                  console.log("this is the keyword list ", tempKeywordList2);
-                  resolve(1);
-                } else {
-                  resolve(0);
-                }
-              } else {
-                resolve(0);
-              }
-              // output['keywords'] = { url: mapped[i], response: JSON.stringify(response, null, 4), results: response['keywords'] };
-
-            }
-          });
-
+      if (tempData.keywords) {
+        var tempKeywordList = [];
+        var tempData2 = tempData.keywords;
+        tempData2.forEach(function(obj) {
+          if (obj.text) {
+            tempKeywordList.push(obj.text);
+          }
         });
-        keywordList.push(promise);
-      });
-
-
-      Promise.all(keywordList).then(function(results) {
-        console.log('keyword list is: ', keywordList);
-        var reduced = ((results.reduce(function(a, b) {
-          return a + b;
-        }, 0)) / results.length);
-        console.log('were done', results);
-
-        cb(reduced);
-      });
-
-      // alchemyapi.keywords('url', parsedString, { 'sentiment': 1 }, function(response) {
-      //   var tempData = response;
-      //   console.log(tempData);
-      //   var tempData2 = tempData.keywords;
-      //   tempDataText = tempData2.map(function(obj) {
-      //     return obj.text;
-      //   })
-      //   console.log(tempDataText);
-      //   output['keywords'] = { url: parsedString, response: JSON.stringify(response, null, 4), results: response['keywords'] };
-      //   // concepts(req, res, output);
-
+        var tempKeywordList2 = tempKeywordList.toString().toLowerCase();
+        if (tempKeywordList2.includes(keyword)) {
+          if (tempKeywordList2.includes(item)) {
+            resolve(1);
+          } else {
+            resolve(0);
+          }
+        } else {
+          resolve(0);
+        }
+      }
     });
-  }, 5000);
 
+  });
+
+  // alchemyapi.keywords('url', parsedString, { 'sentiment': 1 }, function(response) {
+  //   var tempData = response;
+  //   console.log(tempData);
+  //   var tempData2 = tempData.keywords;
+  //   tempDataText = tempData2.map(function(obj) {
+  //     return obj.text;
+  //   })
+  //   console.log(tempDataText);
+  //   output['keywords'] = { url: parsedString, response: JSON.stringify(response, null, 4), results: response['keywords'] };
+  //   // concepts(req, res, output);
 };
 
 // function concepts(req, res, output) {
