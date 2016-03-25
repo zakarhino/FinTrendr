8b35bf643edce140be5f0287c30fbb074ac050e4
Jason Cheung <jasoncheungcf@gmail.com>
2016-03-24 17:03:26 -0700

    (NavBar( Modify Navbar search box

--- shared/components/app.jsx
+++ shared/components/app.jsx
@@ -4,21 +4,41 @@ import SearchBar from './search_bar';
 export default class App extends Component {
   constructor(props){
     super(props);
-    console.log(this.state);
+    //console.log(this.state);
   }
+  componentWillReceiveProps(nextProps)
+  {
+    console.log('App getting new props',nextProps);
+  }
+  componentDidMount (){
+  }
+  compontentWillUpdate()
+  {
+    console.log('app getting updated');
+    return true;
+  }
+  renderSearchBar(){
+    if (this.props.location.pathname==='/'){
+      return (<div></div>)
+    }
+    else {
+      return <SearchBar />
+    }
+  }
+
 
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark navbar-fixed-top bg-inverse">
-          <img src="/img/BarsWhite.png" className="pull-xs-left" width="70"/>
-          <h1 className="mainTitle"><a href="/" className="title-link">Trendr</a></h1>
-          <h6 className="subTitle"><a href="/" className="title-link">Lets Get Trending</a></h6>
-          <div className="row">
-          <div className="col-md-offset-5 search">
-            <SearchBar />
-          </div>
-          </div>
+        <nav className="navbar navbar-dark navbar-fixed-top bg-inverse bottomGreen">
+          <a href="/" className="title-link col-md-2 col-sm-4">
+            <img src="/img/BarsWhite.png" width="70" className="pull-xs-left" />
+            <h1 className="mainTitle">Trendr</h1>
+            <div className="subTitle">Lets Get Trending</div>
+          </a>
+          <form className="navbar-form navbar-right search" role="search">
+              {this.renderSearchBar()}
+          </form>
         </nav>
           {this.props.children}
       </div>
