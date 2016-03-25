8264a8f3ab9e3e0413448261cd9db14e784b5383
Jason Cheung <jasoncheungcf@gmail.com>
2016-03-24 17:03:26 -0700

    (NavBar( Modify Navbar search box

--- shared/components/app.jsx
+++ shared/components/app.jsx
@@ -1,27 +1,52 @@
 import React, { Component } from 'react';
 import SearchBar from './search_bar';
+import {Link} from 'react-router';
 
 export default class App extends Component {
   constructor(props){
     super(props);
-    console.log(this.state);
+    //console.log(this.state);
   }
+  componentWillReceiveProps(nextProps)
+  {
+    console.log('App getting new props',nextProps);
+  }
+  componentDidMount (){
+  }
+  compontentWillUpdate()
+  {
+    console.log('app getting updated');
+    return true;
+  }
+  renderSearchBar(){
+    if (this.props.location.pathname==='/'){
+      return (<div></div>)
+    }
+    else {
+      return <SearchBar />
+    }
+  }
+
 
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark navbar-fixed-top bg-inverse">
-          <img src="/img/BarsWhite.png" className="pull-xs-left" width="70"/>
-          <h1 className="mainTitle"><a href="/" className="title-link">Trendr</a></h1>
-          <h6 className="subTitle"><a href="/" className="title-link">Lets Get Trending</a></h6>
-          <div className="row">
-          <div className="col-md-offset-5 search">
-            <SearchBar />
-          </div>
-          </div>
-          <a href="about" className="info-icon">
-            <img src="/img/InfoWhite.png" width="30" />
+        <nav className="navbar navbar-dark navbar-fixed-top bg-inverse bottomGreen">
+          <a href="/" className="title-link col-md-2 col-sm-4">
+            <img src="/img/BarsWhite.png" width="70" className="pull-xs-left" />
+            <h1 className="mainTitle">Trendr</h1>
+            <div className="subTitle">Lets Get Trending</div>
           </a>
+          <ul className="nav navbar-nav navbar-right">
+            <li><form className="navbar-form search" role="search">
+                {this.renderSearchBar()}
+            </form></li>
+          <li>
+            <Link to="/about" className="info-icon">
+              <img src="/img/InfoWhite.png" width="30" />
+            </Link>
+            </li>
+          </ul>
         </nav>
           {this.props.children}
       </div>
